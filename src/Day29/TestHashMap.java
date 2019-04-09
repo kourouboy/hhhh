d6ec58f2f46c8c44c8733f4650a89db77083f6d6
kourou boy <1376483126@qq.com>
2019-04-09 19:29:12 +0800

    集合框架 -- Map集合 -- HashMapzil

---
+++ src/Day29/TestHashMap.java
@@ -0,0 +1,29 @@
+package Day29;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class TestHashMap {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"A");
+        map.put(1,"A");
+        map.put(3,"C");
+        map.put(2,"B");
+
+//        System.out.println(map);
+//
+//        System.out.println(map.get(2));
+//
+//        System.out.println(map.get(8));
+        //取得map中所有key的信息    输出是有序的，并且不能重复
+
+        Set<Integer> set = map.keySet();
+        Iterator<Integer> iterator = set.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
