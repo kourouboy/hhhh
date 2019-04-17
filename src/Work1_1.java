026051bd4bd64e97de251d02549fcaa44b3972e1
kourou boy <1376483126@qq.com>
2019-04-17 22:59:36 +0800

    work1-1

---
+++ src/Work1_1.java
@@ -0,0 +1,32 @@
+package Array;
+
+
+public class Work1_1 {
+
+    public static boolean Find(int target, int [][] array){
+
+
+        for (int i = 0; i <array.length ; i++) {
+            int low = 0;
+            int high = array[i].length;
+            while(low <= high){
+                int mid = (low + high)/2;
+                if(target > array[i][mid]){
+                    low = mid +1;
+                }else if (target < array[i][mid]){
+                    high = mid -1;
+                }else{
+                    return true;
+                    }
+                }
+            }
+            return false;
+    }
+
+    public static void main(String[] args) {
+//        int[][] data = new int[][] {{1,2,3},{2,3,4}};
+//        int i = 1;
+//        System.out.println(Find(i,data));
+
+    }
+}
